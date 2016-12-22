90eeb55a25ded99179149f8c4ff9ba75a86cd303
lshapz <laurashapiro01@gmail.com>
2016-12-22 16:27:46 -0500

    Done.

--- lib/find-repeated-number.rb
+++ lib/find-repeated-number.rb
@@ -1,3 +1,11 @@
 def find_repeated_number(array)
+  array.each_with_index do |x, i|
+    array.each_with_index do |y, d| 
+      if x == y && i != d
+        return x
+        break
+      end
+    end
+  end
 
 end
